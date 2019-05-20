ddd51fd2a64104e451d80c27d0a1ecb568189ae2
Dirc <e.e.cornet@gmail.com>
2019-05-20 13:39:50 +0200

    add private key example

---
+++ init.groovy.d/add_credentials_privatekey.groovy
@@ -0,0 +1,41 @@
+import jenkins.*
+import hudson.*
+import hudson.model.*
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+
+
+// SECURITY WARNING: Dirty way to get secret
+// FOR DEVELOPMENT ONLY
+// NOTE: For real use, there should be proper secret management.
+
+
+def instance = Jenkins.getInstance()
+
+
+// General Variables
+credentialId="credential_reference_id_key"
+credentialDescription="Example credential"
+credentialUser="FooBar"
+keyPassphrase=""
+// Key options: DirectEntryPrivateKeySource
+// Put full PEM key here:
+key=""
+
+
+global_domain = Domain.global()
+credentials_store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+credentials = new BasicSSHUserPrivateKey(
+        CredentialsScope.GLOBAL,
+        credentialId,
+        credentialUser,
+        // Add correct key option here:
+        new BasicSSHUserPrivateKey.DirectEntryPrivateKeySource(key),
+        keyPassphrase,
+        credentialDescription)
+
+credentials_store.addCredentials(global_domain, credentials)
