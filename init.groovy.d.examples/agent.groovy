c95615918cb265277ba58371384b9de00a9e0569
Dirc <e.e.cornet@gmail.com>
2019-06-06 08:37:32 +0200

    improve agent example

--- init.groovy.d.examples/agent.groovy
+++ init.groovy.d.examples/agent.groovy
@@ -18,15 +18,26 @@ agentHome="/var/jenkins_home"
 agentNumExecutors=1
 
 
-// Pick one of the strategies from the comments below this line
-SshHostKeyVerificationStrategy hostKeyVerificationStrategy = new NonVerifyingKeyVerificationStrategy()
-//= new KnownHostsFileKeyVerificationStrategy() // Known hosts file Verification Strategy
-//= new ManuallyProvidedKeyVerificationStrategy("<your-key-here>") // Manually provided key Verification Strategy
-//= new ManuallyTrustedKeyVerificationStrategy(false /*requires initial manual trust*/) // Manually trusted key Verification Strategy
-//= new NonVerifyingKeyVerificationStrategy() // Non verifying Verification Strategy
-
+// Define a "Permanent Agent"
+Slave agent = new DumbSlave(
+        agentName,
+        agentHome,
+        launcher)
+agent.nodeDescription = agentName
+agent.numExecutors = agentNumExecutors
+agent.labelString = agentName
+agent.mode = Node.Mode.EXCLUSIVE // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
+agent.retentionStrategy = new RetentionStrategy.Always()
 
 // Define a "Launch method": "Launch slave agents via SSH"
+
+// Pick one of the strategies from the comments below this line
+SshHostKeyVerificationStrategy hostKeyVerificationStrategy = new NonVerifyingKeyVerificationStrategy()
+//= new KnownHostsFileKeyVerificationStrategy()
+//= new ManuallyProvidedKeyVerificationStrategy("<your-key-here>")
+//= new ManuallyTrustedKeyVerificationStrategy(false /*requires initial manual trust*/)
+//= new NonVerifyingKeyVerificationStrategy()
+
 ComputerLauncher launcher = new hudson.plugins.sshslaves.SSHLauncher(
         agentHost, // Host
         22, // Port
@@ -41,23 +52,10 @@ ComputerLauncher launcher = new hudson.plugins.sshslaves.SSHLauncher(
         hostKeyVerificationStrategy // Host Key Verification Strategy
 )
 
-
-// Define a "Permanent Agent"
-Slave agent = new DumbSlave(
-        agentName,
-        agentHome,
-        launcher)
-agent.nodeDescription = agentName
-agent.numExecutors = agentNumExecutors
-agent.labelString = agentName
-agent.mode = Node.Mode.EXCLUSIVE // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
-agent.retentionStrategy = new RetentionStrategy.Always()
-
-
 // Create a "Permanent Agent"
 Jenkins.instance.addNode(agent)
 
-return "Node has been created successfully."
+println("Node has been created successfully.")
 
 
 // Connect agent
@@ -68,4 +66,4 @@ for (slave in hudson.model.Hudson.instance.slaves) {
     }
 }
 
-return "Node has been connected successfully."
+println("Node has been connected successfully.")
