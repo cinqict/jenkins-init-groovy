ab428cdebd234f0dffb52bb674ea9e7ed052080a
Dirc <e.e.cornet@gmail.com>
2019-07-18 12:45:59 +0200

    fix email groovy

--- init.groovy.d.examples/email.groovy
+++ init.groovy.d.examples/email.groovy
@@ -10,7 +10,12 @@ String smtpServer = "smpt.example.com"
 String smtpPort = "25"
 String AdminAddress = "jenkins_admin@example.com"        // senders email adress
 
+
 def jenkins = Jenkins.getInstance()
+def mailServer = jenkins.getDescriptor("hudson.tasks.Mailer")
+// extmailServer is not needed here since it inherits from mailServer and we only use default parameters
+// def extmailServer = jenkins.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
+
 
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
 //Jenkins Location
@@ -18,17 +23,9 @@ println "--> Configuring JenkinsLocation"
 jenkinsLocationConfiguration.setAdminAddress(AdminAddress)
 jenkinsLocationConfiguration.save()
 
-// Simple default mail configuration
-//def mailServer = jenkins.getDescriptor("hudson.tasks.Mailer")
-//mailServer.setSmtpHost(smtpServer)
-//mailServer.setSmtpPort(smtpPort)
 
-// Use Email Extension plugin
-// extmailServer is not needed here since it inherits from mailServer and we only use default parameters
-def extmailServer = jenkins.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
-extmailServer.setSmtpServer(smtpServer)
-extmailServer.setSmtpPort(smtpPort)
-
+mailServer.setSmtpHost(smtpServer)
+mailServer.setSmtpPort(smtpPort)
 println("--> SMTP server configured")
 
 jenkins.save()
